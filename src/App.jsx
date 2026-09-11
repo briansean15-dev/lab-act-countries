f481e04775ecb58b33643c36475ded1e906cee4f
JeffKaneki <jeffkaneki121@gmail.com>
2026-09-11 16:20:42 +0800

    Task 4: refactor to layout route + Outlet + index route

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ import HomePage from "./pages/HomePage";
 import CountriesPage from "./pages/CountriesPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
-
 const App = () => {
  return (
  <Routes>
- <Route path="/" element={<Layout><HomePage /></Layout>} />
- <Route path="/countries" element={<Layout><CountriesPage /></Layout>} /
->
- <Route path="/bucket-list" element={<Layout><BucketListPage /></Layout
->} />
- <Route path="/about" element={<Layout><AboutPage /></Layout>} />
+ <Route path="/" element={<Layout />}>
+ <Route index element={<HomePage />} />
+ <Route path="countries" element={<CountriesPage />} />
+ <Route path="bucket-list" element={<BucketListPage />} />
+ <Route path="about" element={<AboutPage />} />
+ </Route>
  </Routes>
  );
 };
